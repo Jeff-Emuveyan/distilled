661b5af1ab06ab67ad04f6c135b6f208822d9600
LAPTOP-PHAJIQ3D\Jeff <jeffemuveyan@gmail.com>
2023-05-05 00:31:18 +0100

    added some dependencies

--- feature/movies/build.gradle.kts
+++ feature/movies/build.gradle.kts
@@ -30,6 +30,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.5"
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -43,13 +49,21 @@ dependencies {
 
     implementation(libs.viewmodel.lifcycle)
     implementation(libs.androidx.core)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.google.material)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test)
     androidTestImplementation(libs.androidx.espresso)
+    androidTestImplementation(libs.androidx.compose.ui.junit)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test)
 
     // hilt
     implementation(libs.hilt)
     kapt(libs.hilt.compiler)
+    // hilt navigation
+    implementation(libs.hilt.navigation.compose)
 }
--- feature/movies/src/main/java/com/example/movies/ui/MoviesScreen.kt
+++ feature/movies/src/main/java/com/example/movies/ui/MoviesScreen.kt
@@ -1,2 +1,9 @@
 package com.example.movies.ui
 
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun MoviesScreen(movieViewModel: MovieViewModel = hiltViewModel()) {
+
+}
