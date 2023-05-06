9da667bcc6ccfbcd290217b9e35624bec1de8a6f
LAPTOP-PHAJIQ3D\Jeff <jeffemuveyan@gmail.com>
2023-05-06 11:23:12 +0100

    tests for MovieViewModelTest.kt

--- feature/movies/src/test/java/com/example/movies/MovieViewModelTest.kt
+++ feature/movies/src/test/java/com/example/movies/MovieViewModelTest.kt
@@ -4,6 +4,7 @@ import com.example.data.repository.IMovieRepository
 import com.example.domain.FormatMovieListResponseUseCase
 import com.example.model.response.MovieResponse
 import com.example.model.response.Response
+import com.example.movies.model.MovieListSortingStyle
 import com.example.movies.model.MovieScreenUiState
 import com.example.movies.ui.MovieViewModel
 import io.mockk.coEvery
@@ -61,6 +62,48 @@ internal class MovieViewModelTest {
         assertIs<MovieScreenUiState.Success>(viewModel.uiState.value)
     }
 
+    @Test
+    fun whenSortStyleIsAlphabetical_moviesListShouldBeSortedAlphabetically() = runTest {
+        val dispatcher = UnconfinedTestDispatcher(testScheduler)
+        viewModel = MovieViewModel(dispatcher, formatMovieListUseCase, repository)
+
+        coEvery { repository.getMovies(any()) } returns getSuccessfulResponseTestData()
+
+        // get the movies
+        viewModel.getMovies(1)
+
+        // apply alphabetical sorting:
+        viewModel.sort(MovieListSortingStyle.ALPHABETICALLY)
+
+        // get the sorted list
+        val sortedList = (viewModel.uiState.value as MovieScreenUiState.Success).list
+        // assert
+        assertEquals("Anaconda", sortedList[0].name)
+        assertEquals("Beauty and the Beast", sortedList[1].name)
+        assertEquals("Lord of the Rings", sortedList[2].name)
+    }
+
+    @Test
+    fun whenSortStyleIsByVoteAverage_moviesListShouldBeSortedByVoteAverage() = runTest {
+        val dispatcher = UnconfinedTestDispatcher(testScheduler)
+        viewModel = MovieViewModel(dispatcher, formatMovieListUseCase, repository)
+
+        coEvery { repository.getMovies(any()) } returns getSuccessfulResponseTestData()
+
+        // get the movies
+        viewModel.getMovies(1)
+
+        // apply alphabetical sorting:
+        viewModel.sort(MovieListSortingStyle.BY_VOTE_AVERAGE)
+
+        // get the sorted list
+        val sortedList = (viewModel.uiState.value as MovieScreenUiState.Success).list
+        // assert
+        assertEquals("Beauty and the Beast", sortedList[0].name)
+        assertEquals("Lord of the Rings", sortedList[1].name)
+        assertEquals("Anaconda", sortedList[2].name)
+    }
+
     private fun getSuccessfulResponseTestData(): Response {
         return Response(
             page = 1,
@@ -71,11 +114,19 @@ internal class MovieViewModelTest {
                     "Description",
                     "www.wwe.com",
                     "2020-04-30",
-                    9.9
+                    9.1
                 ),
                 MovieResponse(
                     2,
-                    "King Kong",
+                    "Anaconda",
+                    "Description",
+                    "www.wwe.com",
+                    "2020-05-31",
+                    0.1
+                ),
+                MovieResponse(
+                    2,
+                    "Beauty and the Beast",
                     "Description",
                     "www.wwe.com",
                     "2020-05-31",
